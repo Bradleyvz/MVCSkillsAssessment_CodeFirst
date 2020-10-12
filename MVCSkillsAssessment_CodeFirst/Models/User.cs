0d64d6d4a23a95722c9d6d24fed40d3029d8e743
Bradley Van Zyl <bradleyvanzyl2@gmail.com>
2020-10-12 14:04:24 +0200

    Added Model Context and User Model

---
+++ MVCSkillsAssessment_CodeFirst/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCSkillsAssessment_CodeFirst.Models
+{
+    public class User
+    {
+    }
+}
