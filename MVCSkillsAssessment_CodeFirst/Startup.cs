e0f7f671827ba9cbffc63ae5e876795dc231181c
Bradley Van Zyl <bradleyvanzyl2@gmail.com>
2020-10-12 13:48:06 +0200

    Test Commit

---
+++ MVCSkillsAssessment_CodeFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCSkillsAssessment_CodeFirst.Startup))]
+namespace MVCSkillsAssessment_CodeFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
