0f3bdced74657a30f22d47e57b71b8135d552cdb
Bradley Van Zyl <bradleyvanzyl2@gmail.com>
2020-10-12 14:13:26 +0200

    Added Connection String to web.Config Added DbContext Class Created User Model With Validation Rules

--- MVCSkillsAssessment_CodeFirst/Models/DataContext.cs
+++ MVCSkillsAssessment_CodeFirst/Models/DataContext.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace MVCSkillsAssessment_CodeFirst.Models
 {
-    public class DataContext
+    public class DataContext:DbContext
     {
+        public DataContext() : base("name=DataContext") { }
+           
+        public DbSet<User> users { get; set; }
+
+        }
     }
-}
--- MVCSkillsAssessment_CodeFirst/Models/User.cs
+++ MVCSkillsAssessment_CodeFirst/Models/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,55 @@ namespace MVCSkillsAssessment_CodeFirst.Models
 {
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserID { get; set; }
+
+        [Display(Name = "Name")]
+        [Required(ErrorMessage = "Name is Required.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Surname")]
+        [Required(ErrorMessage = "Last Name  is Required.")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email is Required.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Password is Required.")]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"? -i)(?= ^.{8,}$)((?!.*\s)(?=.*[A - Z])(?=.*[a - z]))(?=(1)(?=.*\d)|.*[^A - Za - z0 - 9])^.*$", ErrorMessage = "Password must be atleast 8 example Abcd!@un")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [Required(ErrorMessage = "Confirm Password is Required.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password does not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Country ")]
+        public string Country { get; set; }
+
+        [Display(Name = "Favourite Colour ")]
+        public string FavouriteColour { get; set; }
+
+        [Display(Name = "Birthday ")]
+        [DataType(DataType.Date)]
+        public string Birthday { get; set; }
+
+        [Display(Name = "Cellphone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [MaxLength(10), MinLength(10)]
+        public string Cellphone { get; set; }
+
+        [Display(Name = "Comments")]
+        [DataType(DataType.MultilineText)]
+        public string Comments { get; set; }
+
+        public int test { get; set; }
     }
 }
+    
